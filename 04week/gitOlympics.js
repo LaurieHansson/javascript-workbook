4b479b4bcabbdc7a48b56e7dab93a7ecf8557902
LHansson <lhanssonatx@gmail.com>
2018-08-02 19:22:04 -0500

    first

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,9 @@
+'use strict' 
+const PrintListOfOlympians=(inputArr)=>{
+inputArray.forEach((item)=>{
+console.log(input);
+});
+}
+
+const name=['Michael Phelps','Ussain Bolt','Muhammad Ali','Laurie Hansson', 'Bridget Fischer']
+console.log(PrintListOfOlympians(names));
